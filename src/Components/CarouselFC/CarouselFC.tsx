3ca88ff94e5a71aa8e955abefcfa93a5137752a2
Santosh M <hi.santoshm@gmail.com>
2022-08-22 17:09:54 +0530

    refactor: :art: Update site name and renamed CarouselFC style file

--- src/Components/CarouselFC/CarouselFC.tsx
+++ src/Components/CarouselFC/CarouselFC.tsx
@@ -3,7 +3,7 @@ import Row from "react-bootstrap/Row";
 import { Button, Carousel, Container, Figure } from "react-bootstrap";
 import useMediaType, { MediaType } from "../../Hooks/useMediaType";
 
-import styles from "./index.module.css";
+import styles from "./CarouselFC.module.css";
 import useLanguages from "../../Hooks/useLanguages";
 import Rating from "../Rating";
 import { Link } from "react-router-dom";
@@ -11,7 +11,7 @@ import { Link } from "react-router-dom";
 function CarouselFC() {
   const trending: MediaType[] = useMediaType(
     "discover/movie",
-    20,
+    2,
     "&sort_by=release_date.desc&vote_average.gte=7.5&vote_count.gte=10"
   );
   const languages = useLanguages();
@@ -20,48 +20,46 @@ function CarouselFC() {
     <Carousel fade controls={false} indicators={false}>
       {trending.map((item) => (
         <Carousel.Item key={item.id}>
-          <Container fluid>
-            <Row>
-              <Col md={4}>
-                <Carousel.Caption bsPrefix={styles.carouselCaption}>
-                  <Figure.Image
-                    className={styles.figureImg}
-                    src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
-                  />
-                  <h2>
-                    {item.name || item.title} ({item.release_date?.split("-")[0]})
-                  </h2>
-                  <p>
-                    <Rating vote_average={item.vote_average} />
-                    <i className="bi bi-dot"></i>
-                    {` `}
-                    {languages[item.original_language]}
-                  </p>
-                  <p className={styles.overview}>{item.overview}</p>
-                  <div className="actions">
-                    {/* <Button variant="light">
+          <Row>
+            <Col md={4}>
+              <Carousel.Caption bsPrefix={styles.carouselCaption}>
+                <Figure.Image
+                  className={styles.figureImg}
+                  src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
+                />
+                <h2>
+                  {item.name || item.title} ({item.release_date?.split("-")[0]})
+                </h2>
+                <p>
+                  <Rating vote_average={item.vote_average} />
+                  <i className="bi bi-dot"></i>
+                  {` `}
+                  {languages[item.original_language]}
+                </p>
+                <p className={styles.overview}>{item.overview}</p>
+                <div className="actions">
+                  {/* <Button variant="light">
                       <i className="bi bi-play-fill"></i> <span>Watch Trailer</span>
                     </Button>
                     {` `} */}
-                    <Link to={`/movie/${item.id}`}>
-                      <Button variant="light">
-                        <i className="bi bi-info-circle"></i> <span>More Info</span>
-                      </Button>
-                    </Link>
-                  </div>
-                </Carousel.Caption>
-              </Col>
-              <Col>
-                <Figure className={styles.figure}>
-                  <div className={styles.carouselFigure}></div>
-                  <Figure.Image
-                    className="d-block w-100"
-                    src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
-                  />
-                </Figure>
-              </Col>
-            </Row>
-          </Container>
+                  <Link to={`/movie/${item.id}`}>
+                    <Button variant="light">
+                      <i className="bi bi-info-circle"></i> <span>More Info</span>
+                    </Button>
+                  </Link>
+                </div>
+              </Carousel.Caption>
+            </Col>
+            <Col md={6}>
+              <Figure className={styles.figure}>
+                <div className={styles.carouselFigure}></div>
+                <Figure.Image
+                  className="d-block w-100"
+                  src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+                />
+              </Figure>
+            </Col>
+          </Row>
         </Carousel.Item>
       ))}
     </Carousel>
