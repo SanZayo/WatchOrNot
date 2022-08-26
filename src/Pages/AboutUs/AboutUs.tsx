bd4781a098537aa6e90e11edd49ef33cbb987a93
Santosh M <hi.santoshm@gmail.com>
2022-08-26 09:01:03 +0530

    feat: :sparkles: Added About US view

--- src/Pages/AboutUs/AboutUs.tsx
+++ src/Pages/AboutUs/AboutUs.tsx
@@ -1,5 +1,14 @@
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div className="mt-5 pt-5" style={{ textAlign: "center" }}>
+      <img src="/hithere.gif" alt="Hello there"></img>
+      <br />
+      <br />
+      This site was built using React, React Bootstrap, TypeScript and TMDB APIs.
+      <br />
+      You can find the complete source code on Github.
+    </div>
+  );
 }
 
 export default AboutUs;
